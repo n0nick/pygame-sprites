a010b9fc96d2dafb3d9656239f01cac97ccf0ce9
Sagie Maoz <sagie@maoz.info>
2012-06-23 17:43:19 +0300

    Copying original Sprite class from Pygame.

---
+++ sprite.py
@@ -0,0 +1,118 @@
+import pygame
+from operator import truth
+
+
+class Sprite(object):
+    """simple base class for visible game objects
+
+    pygame.sprite.Sprite(*groups): return Sprite
+
+    The base class for visible game objects. Derived classes will want to
+    override the Sprite.update() method and assign Sprite.image and Sprite.rect
+    attributes.  The initializer can accept any number of Group instances that
+    the Sprite will become a member of.
+
+    When subclassing the Sprite class, be sure to call the base initializer
+    before adding the Sprite to Groups.
+
+    """
+
+    def __init__(self, *groups):
+        self.__g = {}  # The groups the sprite is in
+        if groups:
+            self.add(*groups)
+
+    def add(self, *groups):
+        """add the sprite to groups
+
+        Sprite.add(*groups): return None
+
+        Any number of Group instances can be passed as arguments. The
+        Sprite will be added to the Groups it is not already a member of.
+
+        """
+        has = self.__g.__contains__
+        for group in groups:
+            if hasattr(group, '_spritegroup'):
+                if not has(group):
+                    group.add_internal(self)
+                    self.add_internal(group)
+            else:
+                self.add(*group)
+
+    def remove(self, *groups):
+        """remove the sprite from groups
+
+        Sprite.remove(*groups): return None
+
+        Any number of Group instances can be passed as arguments. The Sprite
+        will be removed from the Groups it is currently a member of.
+
+        """
+        has = self.__g.__contains__
+        for group in groups:
+            if hasattr(group, '_spritegroup'):
+                if has(group):
+                    group.remove_internal(self)
+                    self.remove_internal(group)
+            else:
+                self.remove(*group)
+
+    def add_internal(self, group):
+        self.__g[group] = 0
+
+    def remove_internal(self, group):
+        del self.__g[group]
+
+    def update(self, *args):
+        """method to control sprite behavior
+
+        Sprite.update(*args):
+
+        The default implementation of this method does nothing; it's just a
+        convenient "hook" that you can override. This method is called by
+        Group.update() with whatever arguments you give it.
+
+        There is no need to use this method if not using the convenience
+        method by the same name in the Group class.
+
+        """
+        pass
+
+    def kill(self):
+        """remove the Sprite from all Groups
+
+        Sprite.kill(): return None
+
+        The Sprite is removed from all the Groups that contain it. This won't
+        change anything about the state of the Sprite. It is possible to
+        continue to use the Sprite after this method has been called, including
+        adding it to Groups.
+
+        """
+        for c in self.__g:
+            c.remove_internal(self)
+        self.__g.clear()
+
+    def groups(self):
+        """list of Groups that contain this Sprite
+
+        Sprite.groups(): return group_list
+
+        Returns a list of all the Groups that contain this Sprite.
+
+        """
+        return list(self.__g)
+
+    def alive(self):
+        """does the sprite belong to any groups
+
+        Sprite.alive(): return bool
+
+        Returns True when the Sprite belongs to one or more Groups.
+        """
+        return truth(self.__g)
+
+    def __repr__(self):
+        return "<%s sprite(in %d groups)>" \
+                % (self.__class__.__name__, len(self.__g))
