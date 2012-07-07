74c3d760bdf2b4c246590033f6984169e8399bce
Sagie Maoz <sagie@maoz.info>
2012-07-07 22:50:28 +0300

    Adding a new 'attack' demo.
    This demo will demonstrate the AggregatedSprite capabilities.

---
+++ demos/attack.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+import os
+import pygame
+from pygame.locals import *
+from pygame.compat import geterror
+
+# Import new sprite class
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.sys.path.insert(0, parentdir)
+from sprite import *
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+data_dir = main_dir
+
+SCREENSIZE = 700
+BALLROWS = 3
+BALLCOLS = 5
+ROTATE_STEP = 5
+
+colors = {
+        "background": pygame.Color(225, 225, 225)
+}
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join(data_dir, name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error:
+        print ("Cannot load image:", fullname)
+        raise SystemExit(str(geterror()))
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    return image, image.get_rect()
+
+
+class Ball(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.image, self.rect = load_image("ball.png", -1)
+        self.scale = 0.5
+        self.anchor = ANCHOR_CENTER
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREENSIZE, SCREENSIZE))
+    clock = pygame.time.Clock()
+    arrow_keys = [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]
+    rotate_keys = [pygame.K_a, pygame.K_s]
+    quit_keys = [pygame.K_ESCAPE, pygame.K_q]
+
+    # background
+    background = pygame.Surface(screen.get_size(), pygame.SRCALPHA, 32)
+    background = background.convert()
+    background.fill(colors["background"])
+
+    # add ball sprite
+    balls = AggregatedSprite()
+    for i in range(0, BALLCOLS):
+        for j in range(0, BALLROWS):
+            b = Ball()
+            b.position = (50 + i * 50, 50 + j * 50) #TODO
+            balls.add_sprite(b)
+    all = RenderPlain((balls))
+
+    rotate = 0
+    try:
+        while 1:
+
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                        return
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_a:
+                        rotate = 1
+                    elif event.key == pygame.K_s:
+                        rotate = -1
+                elif event.type == pygame.KEYUP:
+                    if event.key in arrow_keys:
+                        pass
+                    elif event.key in rotate_keys:
+                        rotate = 0
+                    elif event.key in quit_keys:
+                        return
+
+            if rotate != 0:
+                balls.rotate += rotate * ROTATE_STEP
+
+            all.clear(screen, background)
+            all.update()
+
+            screen.blit(background, (0, 0))
+
+            all.draw(screen)
+            pygame.display.flip()
+            clock.tick(40)
+    finally:
+        pygame.quit()
+
+if __name__ == "__main__":
+    main()
+
