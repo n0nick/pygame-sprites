aee36fc582e873a3272a35d5d01ea342293e2c24
Sagie Maoz <sagie@maoz.info>
2012-07-07 20:19:25 +0300

    No need to require fonts for stay.py.

--- demos/stay.py
+++ demos/stay.py
@@ -10,10 +10,6 @@ parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.sys.path.insert(0, parentdir)
 from sprite import *
 
-if not pygame.font:
-    print ("No font support compiled")
-    sys.exit()
-
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 data_dir = main_dir
 
