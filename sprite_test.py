79fbf9f032886e542b254e290a366e1359eb125e
Sagie Maoz <sagie@maoz.info>
2012-08-06 21:32:31 +0300

    adding move_to, move_by tests.

--- sprite_test.py
+++ sprite_test.py
@@ -12,9 +12,26 @@ class SpriteTests(unittest.TestCase):
 
     def setUp(self):
         self.shared_stuff = 1
+        self.s1 = Sprite()
+        self.s1.rect = Rect((0, 0, 0, 0))
+        self.s1.position = (0, 0)
 
     def test_sanity(self):
         self.assertEqual(self.shared_stuff, 1)
+
+    def test_move_to(self):
+        self.s1.move_to((0, 0))
+        self.assertEqual(self.s1.position, (0, 0))
+        self.s1.move_to((52, 24))
+        self.assertEqual(self.s1.position, (52, 24))
+
+    def test_move_by(self):
+        self.s1.move_by((0, 0))
+        self.assertEqual(self.s1.position, (0, 0))
+        self.s1.move_by((2, 4))
+        self.assertEqual(self.s1.position, (2, 4))
+        self.s1.move_by((3, 3))
+        self.assertEqual(self.s1.position, (5, 7))
 
 if __name__ == '__main__':
     unittest.main()
