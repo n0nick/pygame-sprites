ba025a2191254af2a966ca1b249647372396b35b
Sagie Maoz <sagie@maoz.info>
2012-08-07 11:33:27 +0300

    Adding offset, visibility tests. Some placeholders.

--- sprite_test.py
+++ sprite_test.py
@@ -33,5 +33,78 @@ class SpriteTests(unittest.TestCase):
         self.s1.move_by((3, 3))
         self.assertEqual(self.s1.position, (5, 7))
 
+    def test_set_image(self):
+        #TODO
+        pass
+
+    def test_update_image(self):
+        #TODO
+        pass
+
+    def test_set_offset(self):
+        self.s1.move_to((0, 0))
+        self.s1.set_offset((25, 25))
+        self.assertEqual(self.s1.rect.topleft, (25, 25))
+        self.s1.move_to((12, 12))
+        self.assertEqual(self.s1.rect.topleft, (37, 37))
+        self.s1.set_offset((7, 7))
+        self.assertEqual(self.s1.rect.topleft, (19, 19))
+
+    def test_make_visible(self):
+        #TODO
+        pass
+
+    def test_make_invisible(self):
+        #TODO
+        pass
+
+    def test_toggle_visibility(self):
+        self.s1.make_visible()
+        self.s1.toggle_visibility()
+        self.assertEqual(self.s1.visible, False)
+        self.s1.toggle_visibility()
+        self.assertEqual(self.s1.visible, True)
+
+    def test_rotate_to(self):
+        #TODO
+        pass
+
+    def test_rotate_by(self):
+        #TODO
+        pass
+
+    def test_scale_to(self):
+        #TODO
+        pass
+
+    def test_scale_by(self):
+        #TODO
+        pass
+
+class AggregatedSpriteTests(unittest.TestCase):
+    def setUp(self):
+        self.s = AggregatedSprite()
+
+    def test_add_sprite(self):
+        s1 = Sprite()
+        s1.rect = Rect((0, 0, 0, 0))
+        s1.position = (0, 0)
+        self.s.add_sprite(s1)
+        self.assertEqual(self.s.sprites, [s1])
+        s2 = Sprite()
+        s2.rect = Rect((0, 0, 0, 0))
+        s2.position = (0, 0)
+        self.s.add_sprite(s2)
+        self.assertEqual(self.s.sprites, [s1, s2])
+
+    def test_draw(self):
+        #TODO
+        pass
+
+    def test_propagate(self):
+        #TODO
+        pass
+
+
 if __name__ == '__main__':
     unittest.main()
